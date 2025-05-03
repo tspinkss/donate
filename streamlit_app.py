e7740990acd44772df64869f4de324d92a43eeb2
tspinkss <65942775+tspinkss@users.noreply.github.com>
2025-05-03 21:55:30 +0000

    Remove unnecessary import of pyfiglet after installing required libraries

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,6 @@ try:
 except ImportError:
     st.warning("Installing required libraries...")
     os.system('pip install pyfiglet user_agent requests')
-    import pyfiglet
     import user_agent
 
 # Function to get reCAPTCHA token
